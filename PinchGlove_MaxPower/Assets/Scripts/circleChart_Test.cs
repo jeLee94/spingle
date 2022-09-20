db9d14badba0d0979c387481c7cc6afb0372ca4a
jay1261 <dongjae73@naver.com>
2022-09-20 14:47:11 +0900

    Fix gauge bar

--- PinchGlove_MaxPower/Assets/Scripts/circleChart_Test.cs
+++ PinchGlove_MaxPower/Assets/Scripts/circleChart_Test.cs
@@ -15,11 +15,11 @@ public class circleChart_Test : MonoBehaviour
     {
         if (b)
         {
-            image.fillAmount = Inputdata.index_F / 2000f;
+            image.fillAmount = Inputdata.index_F / 6000f;
             
             if (progress)
             {
-                progress.text = (int)(image.fillAmount * 2000f) + "";
+                progress.text = (int)(image.fillAmount * 6000f) + "";
             }
         }
     }
